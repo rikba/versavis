9c23f40abea706f4869aedc0816970fd584a67be
Rik Bähnemann <brik@ethz.ch>
2020-02-06 15:04:54 +0100

    Add TCC1

---
+++ firmware/libraries/versavis/src/clock_sync/Tcc1Synced.cpp
@@ -0,0 +1,20 @@
+#include "clock_sync/Tcc1Synced.h"
+
+#include "helper.h"
+
+Tcc1Synced::Tcc1Synced() : TccSynced((Tcc *)TCC1) {
+  // This is a 32 bit counter.
+  top_ = 0xFFFFFFFF;
+  // Enable interrupts. Not as high priority as the RTC interrupt.
+  NVIC_SetPriority(TCC1_IRQn, 0x01);
+  NVIC_EnableIRQ(TCC1_IRQn);
+}
+
+void Tcc1Synced::setupOutPin() {
+  DEBUG_PRINTLN(
+      "[Tcc1Synced]: Configuring port PA07 TCC1/WO[1] wave output pin.");
+  PORT->Group[PORTA].PMUX[7 >> 1].reg |= PORT_PMUX_PMUXO_E;
+  PORT->Group[PORTA].PINCFG[7].reg |= PORT_PINCFG_PMUXEN;
+}
+
+void TCC1_Handler() { DEBUG_PRINTLN("[Tcc1Synced]: TCC1_Handler."); }
---
+++ firmware/libraries/versavis/src/clock_sync/Tcc1Synced.h
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////
+//  January 2020
+//  Author: Rik Bähnemann <brik@ethz.ch>
+////////////////////////////////////////////////////////////////////////////////
+//  Tcc1Synced.h
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef Tcc1Synced_h
+#define Tcc1Synced_h
+
+#include "clock_sync/TccSynced.h"
+
+class Tcc1Synced : public TccSynced {
+public:
+  // Singleton implementation.
+  inline static Tcc1Synced &getInstance() {
+    static Tcc1Synced instance;
+    return instance;
+  }
+  Tcc1Synced(Tcc1Synced const &) = delete;
+  void operator=(Tcc1Synced const &) = delete;
+
+private:
+  Tcc1Synced();
+  void setupOutPin() override;
+};
+
+#endif
