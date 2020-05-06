8f149f14c41d003411d56231a69f5e818474c41c
Rik Bähnemann <brik@ethz.ch>
2020-05-07 01:32:33 +0200

    More comments for CLI built.

--- firmware/setup.sh
+++ firmware/setup.sh
@@ -2,7 +2,8 @@ source ../../../devel/setup.bash
 rm -rf libraries/ros_lib
 rosrun rosserial_arduino make_libraries.py libraries
 
-# Maybe hardcode compiler and bossac path in ~/.arduino15/packages/VersaVIS/hardware/samd/1.0.0/platform.txt
+# Maybe hardcode compiler and bossac path and compiler.arm.cmsis.c.flags in ~/.arduino15/packages/VersaVIS/hardware/samd/1.0.0/platform.txt
+# https://github.com/Open-Bionics/Beetroot/issues/6
 arduino --pref "sketchbook.path=/home/$HOSTNAME/catkin_ws/src/versavis/firmware"
 arduino --pref "boardsmanager.additional.urls=https://github.com/ethz-asl/versavis_hw/raw/master/package_VersaVIS_index.json"
 arduino --install-boards arduino:samd
