30c2c5e2b0541332b8e5b0ab7dc04956450b6271
Rik Bähnemann <brik@ethz.ch>
2020-05-06 23:39:08 +0200

    Add CLI code compilation.

--- firmware/setup.sh
+++ firmware/setup.sh
@@ -1,3 +1,5 @@
 source ../../../devel/setup.bash
 rm -rf libraries/ros_lib
 rosrun rosserial_arduino make_libraries.py libraries
+
+arduino --upload --board VersaVIS:samd:VersaVIS --port /dev/versavis versavis_rtc/versavis_rtc.ino
