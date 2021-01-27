23f707353df7bc3fcd4731249ad599e3792a4091
Rik Bähnemann <brik@ethz.ch>
2021-01-27 17:26:33 +0100

    Make it bash script.

--- firmware/setup.sh
+++ firmware/setup.sh
@@ -1,13 +1,20 @@
+#!/bin/bash
 source ../../../devel/setup.bash
 rm -rf libraries/ros_lib
 rosrun rosserial_arduino make_libraries.py libraries
 
 # Maybe hardcode compiler and bossac path and compiler.arm.cmsis.c.flags in ~/.arduino15/packages/VersaVIS/hardware/samd/1.0.0/platform.txt
 # https://github.com/Open-Bionics/Beetroot/issues/6
+sudo apt install -y arduino
 arduino --pref "sketchbook.path=/home/$HOSTNAME/catkin_ws/src/versavis/firmware"
 arduino --pref "boardsmanager.additional.urls=https://github.com/ethz-asl/versavis_hw/raw/master/package_VersaVIS_index.json"
 arduino --pref "build.path=/home/$HOSTNAME/catkin_ws/src/versavis/firmware/build"
-#arduino --install-boards arduino:samd
-#arduino --install-boards arduino:samd_beta
-#arduino --install-boards VersaVIS:samd
+
+echo "Do you wish to install additional arduino boards? [y or Y to accept]"
+read install_boards
+if [[ $install_boards == "Y" || $install_boards == "y" ]]; then
+  arduino --install-boards arduino:samd
+  arduino --install-boards arduino:samd_beta
+  arduino --install-boards VersaVIS:samd
+fi
 arduino --upload --board VersaVIS:samd:VersaVIS --port /dev/versavis versavis_rtc/versavis_rtc.ino
