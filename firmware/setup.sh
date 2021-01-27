f417a86b1b4ef69a7f137ba2a5d85a165006ed46
Rik Bähnemann <brik@ethz.ch>
2021-01-27 18:18:43 +0100

    Use arduino 1.8.13

--- firmware/setup.sh
+++ firmware/setup.sh
@@ -5,16 +5,23 @@ rosrun rosserial_arduino make_libraries.py libraries
 
 # Maybe hardcode compiler and bossac path and compiler.arm.cmsis.c.flags in ~/.arduino15/packages/VersaVIS/hardware/samd/1.0.0/platform.txt
 # https://github.com/Open-Bionics/Beetroot/issues/6
-sudo apt install -y arduino
-arduino --pref "sketchbook.path=/home/$HOSTNAME/catkin_ws/src/versavis/firmware"
-arduino --pref "boardsmanager.additional.urls=https://github.com/ethz-asl/versavis_hw/raw/master/package_VersaVIS_index.json"
-arduino --pref "build.path=/home/$HOSTNAME/catkin_ws/src/versavis/firmware/build"
+
+echo "Do you wish to install the arduino CLI? [y or Y to accept]"
+read install_arduino
+if [[ $install_boards == "Y" || $install_boards == "y" ]]; then
+  wget https://downloads.arduino.cc/arduino-1.8.13-linux64.tar.xz
+  tar -xf arduino-1.8.13-linux64.tar.xz
+fi
+
+./arduino-1.8.13/arduino --pref "sketchbook.path=/home/$HOSTNAME/catkin_ws/src/versavis/firmware"
+./arduino-1.8.13/arduino --pref "boardsmanager.additional.urls=https://github.com/ethz-asl/versavis_hw/raw/master/package_VersaVIS_index.json"
+./arduino-1.8.13/arduino --pref "build.path=/home/$HOSTNAME/catkin_ws/src/versavis/firmware/build"
 
 echo "Do you wish to install additional arduino boards? [y or Y to accept]"
 read install_boards
 if [[ $install_boards == "Y" || $install_boards == "y" ]]; then
-  arduino --install-boards arduino:samd
-  arduino --install-boards arduino:samd_beta
-  arduino --install-boards VersaVIS:samd
+  ./arduino-1.8.13/arduino --install-boards arduino:samd
+  ./arduino-1.8.13/arduino --install-boards arduino:samd_beta
+  ./arduino-1.8.13/arduino --install-boards VersaVIS:samd
 fi
-arduino --upload --board VersaVIS:samd:VersaVIS --port /dev/versavis versavis_rtc/versavis_rtc.ino
+./arduino-1.8.13/arduino --upload --board VersaVIS:samd:VersaVIS --port /dev/versavis versavis_rtc/versavis_rtc.ino
